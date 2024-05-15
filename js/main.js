0d33b86358287c4a7904419d06c2056ede87eed7
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-15 17:56:18 +0200

    added bonus-2 with Input and button that generated arrayMail with value input

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
+// const { createApp } = Vue
+
+// createApp({
+//   data() {
+//     return {
+//       arrayMail: [],
+//     }
+//   },
+//   methods: {
+//     randomMail() {
+//       for (let i = 0; i < 10; i++) {
+//         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+//           this.arrayMail.push(result.data.response)
+//           console.log(this.arrayMail[i])
+//         })
+//       }
+//     }
+//   },
+//   mounted() {
+//     this.randomMail()
+//   }
+// }).mount('#app')
+
+//               PER BONUS 2 CON INPUT CHE SCEGLIE QUANTE MAIL GENERARE
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
       arrayMail: [],
+      userInput : "",
     }
   },
   methods: {
     randomMail() {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < this.userInput; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
           this.arrayMail.push(result.data.response)
           console.log(this.arrayMail[i])
