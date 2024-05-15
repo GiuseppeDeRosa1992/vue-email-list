404e41936944490b8300209fe853c38d9844861a
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-15 17:22:23 +0200

    fixed bug print arrayMail in page

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ createApp({
   },
   methods: {
     randomMail() {
-      for (let i = 0; i < 9; i++) {
+      for (let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-          this.arrayMail = result.data
-          console.log(this.arrayMail.response)
+          this.arrayMail.push(result.data.response)
+          console.log(this.arrayMail[i])
         })
       }
     }
