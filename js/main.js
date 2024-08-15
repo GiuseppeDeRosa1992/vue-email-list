17c9b2aec0993a4f5f4ea85429d971680e5a80d0
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-08-15 17:52:40 +0200

    refactoring code, added if checks numberInput

--- js/main.js
+++ js/main.js
@@ -28,21 +28,32 @@ createApp({
   data() {
     return {
       arrayMail: [],
-      userInput : "",
-      title : "Genera il numero di mail che vuoi"
+      userInput: "",
+      title: "Genera il numero di mail che vuoi",
+      numberInput: "",
     }
   },
   methods: {
+
     randomMail() {
-      for (let i = 0; i < this.userInput; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-          this.arrayMail.push(result.data.response)
-          console.log(this.arrayMail[i])
-        })
+      this.numberInput = this.userInput;
+      this.arrayMail = [];
+      //SE IL NUMERO DELL'UTENTE E MINORE DI 5 STAMPO IN PAGINA UN ALERT
+      if (this.numberInput < 5 || this.numberInput > 250) {
+        alert("Errore, si devono generare tra 5 e 250 mail!");
+      }
+      //SENNò STAMPO LE MAIL TRAMITE CHIAMATA AXIOS
+      else {
+        for (let i = 0; i < this.numberInput; i++) {
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+            this.arrayMail.push(result.data.response)
+            console.log(this.arrayMail[i])
+          })
+        }
       }
     }
   },
   mounted() {
-    this.randomMail()
+    // this.randomMail()
   }
 }).mount('#app')
